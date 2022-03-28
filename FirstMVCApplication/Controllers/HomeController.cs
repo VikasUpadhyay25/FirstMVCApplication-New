18c42382e15cd45ea45e471f6951684d9cc25ba3
pandit ji <pandit ji@DESKTOP-37UDTFK>
2022-03-28 08:43:23 +0530

    Add project files.

---
+++ FirstMVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCApplication.Models;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            //Provider p = new Provider() {
+            //    ProviderId = 1,
+            //    ProviderName = "Vikas",
+            //    ProviderType = "Claim",
+            //};
+
+            Provider p = new Provider();
+
+            var data = p.GetProviderData();
+
+            p.ProviderId = Convert.ToInt32(data.Tables[0].Rows[0][0].ToString());
+            p.ProviderName = (data.Tables[0].Rows[0][1].ToString());
+            p.ProviderType = (data.Tables[0].Rows[0][2].ToString());
+
+            //ViewData["Orders"] = p;
+            //ViewBag.Message = "Good Morning to BMW Official Page";
+            return View(p);
+        }
+    }
+}
---
+++ FirstMVCApplication/Models/Provider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FirstMVCApplication.Models
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public string ProviderType { get; set; }
+
+        public DataSet GetProviderData()
+        {
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C: \Users\pandit ji\source\reposFirstMVCApplication\FirstMVCApplication\FirstMVCApplication\App_Data\ProviderDatabase.mdf";Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+            SqlCommand command = new SqlCommand("select * from Provider", conn);
+            
+            
+            SqlDataAdapter adaptor = new SqlDataAdapter(command);
+
+            DataSet ds = new DataSet();
+
+            adaptor.Fill(ds);
+
+            return ds;
+        }
+    }
+}
